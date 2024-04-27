8faffa3d7fa5e42c25f304db7b15ef9227025c99
elbert <elbertchen007@gmail.com>
2024-04-28 02:05:37 +0700

    main: mengubah footer

--- src/component/element/Footer.jsx
+++ src/component/element/Footer.jsx
@@ -2,7 +2,7 @@
 function Footer(){
     return (
         <footer className='sticky h-11 bg-white bottom-0 flex w-100 justify-center items-center'>
-            <h1 className='font-semibold tracking-wide'>Thanks by Me</h1>
+            <h1 className='font-semibold tracking-wide'>Terima Kasih Telah Mengisi</h1>
         </footer>
     )
 }
