bcfbae376fe36b0c015fa814088d3c24f1cc717e
elbert <elbertchen007@gmail.com>
2024-04-28 02:01:13 +0700

    main: tambah beebrapa pertanyaan

--- src/component/fragment/QuestionForm.jsx
+++ src/component/fragment/QuestionForm.jsx
@@ -10,11 +10,15 @@ function QuestionForm(){
         <form action="" method='post' onSubmit={SubmitFunc}>
             <QuestionBox type='text' placeholder="Nama anda" id='nama' title='Nama' objKey='name'/>
             <QuestionBox type='text' placeholder="Kelas anda" id='kelas' title='Kelas' objKey='class'/>
-            <QuestionBox type='radio' name="first" title='Bagaimana pendapat anda mengenai fasilitas sekolah?'/>
+            <QuestionBox type='radio' name="first" title='Bagaimana pendapat anda mengenai pelaksanaan kurikulum merdeka?'/>
             <QuestionBox type='radio' name="second" title='Seberapa suka anda terhadap kurikulum merdeka?'/>
             <QuestionBox type='radio' name="third" title='Bagaimana tingkat kreatifitas anda setelah menjalani kurikulum merdeka?'/>
             <QuestionBox type='radio' name="forth" title='Bagaimana kesan anda terhadap kurikulum merdeka?'/>
             <QuestionBox type='radio' name="fifth" title='Bagaimana tingkat penggunaan fasilitas sekolah dalam kurikulum merdeka?'/>
+            <QuestionBox type='radio' name="sixth" title='Bagaimana partisipasi anda dalam pembelajaran di kurikulum merdeka?'/>
+            <QuestionBox type='radio' name="seventh" title='Bagaimana tingkat kepuasan anda dalam kurikulum merdeka?'/>
+            <QuestionBox type='radio' name="eighth" title='Bagaimana fleksibilitas yang diberikan oleh kurikulum merdeka dalam pembelajran?'/>
+            <QuestionBox type='radio' name="ninth" title='Bagaimana tingkat keaktifan siswa dalam kurikulum merdeka?'/>
             <Submit />
         </form>
     )
